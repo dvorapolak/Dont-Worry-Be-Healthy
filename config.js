e04c2dedba3507374b6584d0ea97d3eb991d8a0d
dvora 3 <73206607+dvorapolak@users.noreply.github.com>
2022-01-31 19:30:51 -0500

    Add files via upload

---
+++ config.js
@@ -0,0 +1,13 @@
+import firebase from "firebase"
+
+var firebaseConfig = {
+  apiKey: "AIzaSyApZNUXmIc2Wn9lFbIwOeu41Zq8rIm-5gM",
+  authDomain: "storytellingapp-2b192.firebaseapp.com",
+  projectId: "storytellingapp-2b192",
+  storageBucket: "storytellingapp-2b192.appspot.com",
+  messagingSenderId: "159269276839",
+  appId: "1:159269276839:web:0295e23122653eacc05f82"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
